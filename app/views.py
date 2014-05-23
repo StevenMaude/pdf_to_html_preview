a0340a32f9f0495562f28eaa895ff6e3c97ff7b8
Steven Maude <steve@github.ubuntu>
2014-05-23 22:58:40 +0100

    Add basic web app code
    No error handling yet.

--- app/views.py
+++ app/views.py
@@ -2,7 +2,18 @@
 # encoding: utf-8
 from app import app
 
+from flask import render_template, request
+import pdf_to_html
+
 
 @app.route('/')
 def index():
-    return "Hello World"
+    url = request.args.get('url')
+    if url:
+        pages_data, fontspecs, pdf_info = pdf_to_html.main(url)
+        return render_template('output_template.html',
+                               pages_data=pages_data,
+                               fontspecs=fontspecs,
+                               pdf_info=pdf_info)
+    else:
+        return render_template('start_template.html')
